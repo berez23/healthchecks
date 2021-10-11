e92f78cccd04e847dbe19be3d04adca02d8c5d25
Francesco Filicetti <francesco.filicetti@unical.it>
2021-10-11 12:02:36 +0200

    fix: slugify user username

--- hc/accounts/signals.py
+++ hc/accounts/signals.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db.models.signals import *
 from django.dispatch import receiver
+from django.utils.text import slugify
 
 
 @receiver(pre_save, sender=User)
@@ -9,3 +10,11 @@ def user_email_lowercase(sender, instance, **kwargs):
     Make user email lowercase
     """
     instance.email = instance.email.lower()
+
+
+@receiver(pre_save, sender=User)
+def user_username_slugify(sender, instance, **kwargs):
+    """
+    Slugify username to avoid special characters
+    """
+    instance.username = slugify(instance.username)
